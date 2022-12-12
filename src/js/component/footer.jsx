72a7e55e016e6db0173007a667007b9c3220fb07
Daniel Jimenez <daniel.jimenez.cr@gmail.com>
2022-12-12 22:39:45 +0000

    add more padding to footer

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = ()=> {
 
-return <div className="text-white bg-dark">
+return <div className="text-white bg-dark py-2">
   <footer className="py-4">
     <p className="text-center mb-0">Your Website 2022</p>
 </footer>
